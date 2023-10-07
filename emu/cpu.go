ee938c403d9d5fa9a6f4b57b17ebfd240fd335ca
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-10-07 19:30:37 +0100

    Add some cpu instructions
    …with lots to go

--- emu/cpu.go
+++ emu/cpu.go
@@ -17,8 +17,8 @@ type CPU struct {
 
 type INSTRUCTION struct {
 	Name string
-	Operate func(cpu) uint8
-	AddrMode func(cpu) uint8
+	Operate func(*CPU) uint8
+	AddrMode func(*CPU) uint8
 	Cycles uint8
 }
 
@@ -50,8 +50,24 @@ func NewCPU() *CPU {
 	cpu.opcode = 0x00
 	cpu.cycles = 0
 
-	lookup := []INSTRUCTION{
-		{}
+	lookup := []INSTRUCTION
+	{
+		{ "BRK", (*CPU).BRK, (*CPU).IMM, 7 },{ "ORA", (*CPU).ORA, (*CPU).IZX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 3 },{ "ORA", (*CPU).ORA, (*CPU).ZP0, 3 },{ "ASL", (*CPU).ASL, (*CPU).ZP0, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "PHP", (*CPU).PHP, (*CPU).IMP, 3 },{ "ORA", (*CPU).ORA, (*CPU).IMM, 2 },{ "ASL", (*CPU).ASL, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "ORA", (*CPU).ORA, (*CPU).ABS, 4 },{ "ASL", (*CPU).ASL, (*CPU).ABS, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },
+		{ "BPL", (*CPU).BPL, (*CPU).REL, 2 },{ "ORA", (*CPU).ORA, (*CPU).IZY, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "ORA", (*CPU).ORA, (*CPU).ZPX, 4 },{ "ASL", (*CPU).ASL, (*CPU).ZPX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "CLC", (*CPU).CLC, (*CPU).IMP, 2 },{ "ORA", (*CPU).ORA, (*CPU).ABY, 4 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "ORA", (*CPU).ORA, (*CPU).ABX, 4 },{ "ASL", (*CPU).ASL, (*CPU).ABX, 7 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },
+		{ "JSR", (*CPU).JSR, (*CPU).ABS, 6 },{ "AND", (*CPU).AND, (*CPU).IZX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "BIT", (*CPU).BIT, (*CPU).ZP0, 3 },{ "AND", (*CPU).AND, (*CPU).ZP0, 3 },{ "ROL", (*CPU).ROL, (*CPU).ZP0, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "PLP", (*CPU).PLP, (*CPU).IMP, 4 },{ "AND", (*CPU).AND, (*CPU).IMM, 2 },{ "ROL", (*CPU).ROL, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "BIT", (*CPU).BIT, (*CPU).ABS, 4 },{ "AND", (*CPU).AND, (*CPU).ABS, 4 },{ "ROL", (*CPU).ROL, (*CPU).ABS, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },
+		{ "BMI", (*CPU).BMI, (*CPU).REL, 2 },{ "AND", (*CPU).AND, (*CPU).IZY, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "AND", (*CPU).AND, (*CPU).ZPX, 4 },{ "ROL", (*CPU).ROL, (*CPU).ZPX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "SEC", (*CPU).SEC, (*CPU).IMP, 2 },{ "AND", (*CPU).AND, (*CPU).ABY, 4 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "AND", (*CPU).AND, (*CPU).ABX, 4 },{ "ROL", (*CPU).ROL, (*CPU).ABX, 7 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },
+		{ "RTI", (*CPU).RTI, (*CPU).IMP, 6 },{ "EOR", (*CPU).EOR, (*CPU).IZX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 3 },{ "EOR", (*CPU).EOR, (*CPU).ZP0, 3 },{ "LSR", (*CPU).LSR, (*CPU).ZP0, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "PHA", (*CPU).PHA, (*CPU).IMP, 3 },{ "EOR", (*CPU).EOR, (*CPU).IMM, 2 },{ "LSR", (*CPU).LSR, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "JMP", (*CPU).JMP, (*CPU).ABS, 3 },{ "EOR", (*CPU).EOR, (*CPU).ABS, 4 },{ "LSR", (*CPU).LSR, (*CPU).ABS, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },
+		{ "BVC", (*CPU).BVC, (*CPU).REL, 2 },{ "EOR", (*CPU).EOR, (*CPU).IZY, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "EOR", (*CPU).EOR, (*CPU).ZPX, 4 },{ "LSR", (*CPU).LSR, (*CPU).ZPX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "CLI", (*CPU).CLI, (*CPU).IMP, 2 },{ "EOR", (*CPU).EOR, (*CPU).ABY, 4 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "EOR", (*CPU).EOR, (*CPU).ABX, 4 },{ "LSR", (*CPU).LSR, (*CPU).ABX, 7 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },
+		{ "RTS", (*CPU).RTS, (*CPU).IMP, 6 },{ "ADC", (*CPU).ADC, (*CPU).IZX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 3 },{ "ADC", (*CPU).ADC, (*CPU).ZP0, 3 },{ "ROR", (*CPU).ROR, (*CPU).ZP0, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "PLA", (*CPU).PLA, (*CPU).IMP, 4 },{ "ADC", (*CPU).ADC, (*CPU).IMM, 2 },{ "ROR", (*CPU).ROR, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "JMP", (*CPU).JMP, (*CPU).IND, 5 },{ "ADC", (*CPU).ADC, (*CPU).ABS, 4 },{ "ROR", (*CPU).ROR, (*CPU).ABS, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },
+		{ "BVS", (*CPU).BVS, (*CPU).REL, 2 },{ "ADC", (*CPU).ADC, (*CPU).IZY, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "ADC", (*CPU).ADC, (*CPU).ZPX, 4 },{ "ROR", (*CPU).ROR, (*CPU).ZPX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "SEI", (*CPU).SEI, (*CPU).IMP, 2 },{ "ADC", (*CPU).ADC, (*CPU).ABY, 4 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "ADC", (*CPU).ADC, (*CPU).ABX, 4 },{ "ROR", (*CPU).ROR, (*CPU).ABX, 7 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },
+		{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "STA", (*CPU).STA, (*CPU).IZX, 6 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "STY", (*CPU).STY, (*CPU).ZP0, 3 },{ "STA", (*CPU).STA, (*CPU).ZP0, 3 },{ "STX", (*CPU).STX, (*CPU).ZP0, 3 },{ "???", (*CPU).XXX, (*CPU).IMP, 3 },{ "DEY", (*CPU).DEY, (*CPU).IMP, 2 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "TXA", (*CPU).TXA, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "STY", (*CPU).STY, (*CPU).ABS, 4 },{ "STA", (*CPU).STA, (*CPU).ABS, 4 },{ "STX", (*CPU).STX, (*CPU).ABS, 4 },{ "???", (*CPU).XXX, (*CPU).IMP, 4 },
+		{ "BCC", (*CPU).BCC, (*CPU).REL, 2 },{ "STA", (*CPU).STA, (*CPU).IZY, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "STY", (*CPU).STY, (*CPU).ZPX, 4 },{ "STA", (*CPU).STA, (*CPU).ZPX, 4 },{ "STX", (*CPU).STX, (*CPU).ZPY, 4 },{ "???", (*CPU).XXX, (*CPU).IMP, 4 },{ "TYA", (*CPU).TYA, (*CPU).IMP, 2 },{ "STA", (*CPU).STA, (*CPU).ABY, 5 },{ "TXS", (*CPU).TXS, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "???", (*CPU).NOP, (*CPU).IMP, 5 },{ "STA", (*CPU).STA, (*CPU).ABX, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },
+		{ "LDY", (*CPU).LDY, (*CPU).IMM, 2 },{ "LDA", (*CPU).LDA, (*CPU).IZX, 6 },{ "LDX", (*CPU).LDX, (*CPU).IMM, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "LDY", (*CPU).LDY, (*CPU).ZP0, 3 },{ "LDA", (*CPU).LDA, (*CPU).ZP0, 3 },{ "LDX", (*CPU).LDX, (*CPU).ZP0, 3 },{ "???", (*CPU).XXX, (*CPU).IMP, 3 },{ "TAY", (*CPU).TAY, (*CPU).IMP, 2 },{ "LDA", (*CPU).LDA, (*CPU).IMM, 2 },{ "TAX", (*CPU).TAX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "LDY", (*CPU).LDY, (*CPU).ABS, 4 },{ "LDA", (*CPU).LDA, (*CPU).ABS, 4 },{ "LDX", (*CPU).LDX, (*CPU).ABS, 4 },{ "???", (*CPU).XXX, (*CPU).IMP, 4 },
+		{ "BCS", (*CPU).BCS, (*CPU).REL, 2 },{ "LDA", (*CPU).LDA, (*CPU).IZY, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "LDY", (*CPU).LDY, (*CPU).ZPX, 4 },{ "LDA", (*CPU).LDA, (*CPU).ZPX, 4 },{ "LDX", (*CPU).LDX, (*CPU).ZPY, 4 },{ "???", (*CPU).XXX, (*CPU).IMP, 4 },{ "CLV", (*CPU).CLV, (*CPU).IMP, 2 },{ "LDA", (*CPU).LDA, (*CPU).ABY, 4 },{ "TSX", (*CPU).TSX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 4 },{ "LDY", (*CPU).LDY, (*CPU).ABX, 4 },{ "LDA", (*CPU).LDA, (*CPU).ABX, 4 },{ "LDX", (*CPU).LDX, (*CPU).ABY, 4 },{ "???", (*CPU).XXX, (*CPU).IMP, 4 },
+		{ "CPY", (*CPU).CPY, (*CPU).IMM, 2 },{ "CMP", (*CPU).CMP, (*CPU).IZX, 6 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "CPY", (*CPU).CPY, (*CPU).ZP0, 3 },{ "CMP", (*CPU).CMP, (*CPU).ZP0, 3 },{ "DEC", (*CPU).DEC, (*CPU).ZP0, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "INY", (*CPU).INY, (*CPU).IMP, 2 },{ "CMP", (*CPU).CMP, (*CPU).IMM, 2 },{ "DEX", (*CPU).DEX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "CPY", (*CPU).CPY, (*CPU).ABS, 4 },{ "CMP", (*CPU).CMP, (*CPU).ABS, 4 },{ "DEC", (*CPU).DEC, (*CPU).ABS, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },
+		{ "BNE", (*CPU).BNE, (*CPU).REL, 2 },{ "CMP", (*CPU).CMP, (*CPU).IZY, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "CMP", (*CPU).CMP, (*CPU).ZPX, 4 },{ "DEC", (*CPU).DEC, (*CPU).ZPX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "CLD", (*CPU).CLD, (*CPU).IMP, 2 },{ "CMP", (*CPU).CMP, (*CPU).ABY, 4 },{ "NOP", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "CMP", (*CPU).CMP, (*CPU).ABX, 4 },{ "DEC", (*CPU).DEC, (*CPU).ABX, 7 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },
+		{ "CPX", (*CPU).CPX, (*CPU).IMM, 2 },{ "SBC", (*CPU).SBC, (*CPU).IZX, 6 },{ "???", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "CPX", (*CPU).CPX, (*CPU).ZP0, 3 },{ "SBC", (*CPU).SBC, (*CPU).ZP0, 3 },{ "INC", (*CPU).INC, (*CPU).ZP0, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 5 },{ "INX", (*CPU).INX, (*CPU).IMP, 2 },{ "SBC", (*CPU).SBC, (*CPU).IMM, 2 },{ "NOP", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).SBC, (*CPU).IMP, 2 },{ "CPX", (*CPU).CPX, (*CPU).ABS, 4 },{ "SBC", (*CPU).SBC, (*CPU).ABS, 4 },{ "INC", (*CPU).INC, (*CPU).ABS, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },
+		{ "BEQ", (*CPU).BEQ, (*CPU).REL, 2 },{ "SBC", (*CPU).SBC, (*CPU).IZY, 5 },{ "???", (*CPU).XXX, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 8 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "SBC", (*CPU).SBC, (*CPU).ZPX, 4 },{ "INC", (*CPU).INC, (*CPU).ZPX, 6 },{ "???", (*CPU).XXX, (*CPU).IMP, 6 },{ "SED", (*CPU).SED, (*CPU).IMP, 2 },{ "SBC", (*CPU).SBC, (*CPU).ABY, 4 },{ "NOP", (*CPU).NOP, (*CPU).IMP, 2 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },{ "???", (*CPU).NOP, (*CPU).IMP, 4 },{ "SBC", (*CPU).SBC, (*CPU).ABX, 4 },{ "INC", (*CPU).INC, (*CPU).ABX, 7 },{ "???", (*CPU).XXX, (*CPU).IMP, 7 },
 	}
 }
 
@@ -73,7 +89,38 @@ func (cpu *CPU) Write(a uint16, d uint16) error {
 
 // Updates the clock cycle
 func (cpu *CPU) Clock() {
-	;
+	if cpu.cycles == 0 {
+		cpu.opcode = cpu.Read(cpu.pc)
+		cpu.pc++
+
+		cpu.cycles = lookup[opcode].Cycles
+
+		additional_cycle1 := lookup[opcode].AddrMode(cpu)
+
+		additional_cycle2 := lookup[opcode].Operate(cpu)
+
+		cpu.cycles += (additional_cycle1 + additional_cycle2)
+	}
+
+	cpu.cycles--
+}
+
+
+func (cpu *CPU) GetFlag(flag int) uint8 {
+	if (cpu.status & flag) > 0 {
+		return 1
+	}
+	return 0
+}
+
+
+func (cpu *CPU) SetFlag(flag int, v bool) {
+	if v {
+		cpu.status |= flag
+	}
+	else {
+		cpu.status &= ^flag
+	}
 }
 
 
@@ -96,5 +143,398 @@ func (cpu *CPU) NMI() {
 
 
 func (cpu *CPU) Fetch() {
-	;
+	if !(cpu.lookup[cpu.opcode].AddrMode == (*CPU).IMP) {
+		cpu.fetched = cpu.Read(cpu.addr_abs)
+	}
+
+	return fetched
+}
+
+// Addressing Modes
+
+func (cpu *CPU) IMP() uint8 {
+	cpu.fetched = cpu.a
+
+	return 0
+}
+
+func (cpu *CPU) IMM() uint8 {
+	cpu.addr_abs = cpu.pc++
+
+	return 0
+}
+
+func (cpu *CPU) ZP0() uint8 {
+	cpu.addr_abs = cpu.Read(cpu.pc)
+	cpu.pc++
+	addr_abs &= 0x00FF
+
+	return 0
+}
+
+func (cpu *CPU) ZPX() uint8 {
+	cpu.addr_abs = (cpu.Read(cpu.pc) + cpu.x)
+	cpu.pc++
+	addr_abs &= 0x00FF
+
+	return 0
+}
+
+func (cpu *CPU) ZPY() uint8 {
+	cpu.addr_abs = (cpu.Read(cpu.pc) + cpu.y)
+	cpu.pc++
+	addr_abs &= 0x00FF
+
+	return 0
+}
+
+func (cpu *CPU) REL() uint8 {
+	cpu.addr_rel = cpu.Read(cpu.pc)
+	cpu.pc++
+	if cpu.addr_rel & 0x80 {
+		cpu.addr_rel |= 0xFF00
+	}
+
+	return 0
+}
+
+func (cpu *CPU) ABS() uint8 {
+	lo := cpu.Read(cpu.pc)
+	cpu.pc++
+	hi := cpu.Read(cpu.pc)
+	cpu.pc++
+
+	cpu.addr_abs = (hi << 8) | lo
+
+	return 0
+}
+
+func (cpu *CPU) ABX() uint8 {
+	lo := cpu.Read(cpu.pc)
+	cpu.pc++
+	hi := cpu.Read(cpu.pc)
+	cpu.pc++
+
+	cpu.addr_abs = (hi << 8) | lo
+	cpu.addr_abs += cpu.x
+
+	if (cpu.addr_abs & 0xFF00) != (hi << 8) {
+		return 1
+	}
+
+	return 0
+}
+
+func (cpu *CPU) ABY() uint8 {
+	lo := cpu.Read(cpu.pc)
+	cpu.pc++
+	hi := cpu.Read(cpu.pc)
+	cpu.pc++
+
+	cpu.addr_abs = (hi << 8) | lo
+	cpu.addr_abs += cpu.y
+
+	if (cpu.addr_abs & 0xFF00) != (hi << 8) {
+		return 1
+	}
+
+	return 0
+}
+
+func (cpu *CPU) IND() uint8 {
+	ptr_lo := cpu.Read(cpu.pc)
+	cpu.pc++
+	ptr_hi := cpu.Read(cpu.pc)
+	cpu.pc++
+
+	ptr := (ptr_hi << 8) | ptr_lo
+
+	if ptr_lo == 0x00FF {
+		cpu.addr_abs = (cpu.Read(ptr & 0xFF00) << 8) | cpu.Read(ptr + 0)
+	}
+	else {
+		cpu.addr_abs = (cpu.Read(ptr + 1) << 8) | cpu.Read(ptr + 0)
+	}
+
+	return 0
+}
+
+func (cpu *CPU) IZX() uint8 {
+	t := cpu.Read(cpu.pc)
+	cpu.pc++
+
+	lo := cpu.Read(uint16(cpu.t + uint16(cpu.x)) & 0x00FF)
+	hi := cpu.Read(uint16(cpu.t + uint16(cpu.x) + 1) & 0x00FF)
+
+	cpu.addr_abs = (hi << 8) | lo
+
+	return 0
+}
+
+func (cpu *CPU) IZY() uint8 {
+	t := cpu.Read(cpu.pc)
+	cpu.pc++
+
+	lo := cpu.Read(t & 0x00FF)
+	hi := cpu.Read((t + 1) & 0x00FF)
+
+	cpu.addr_abs = (hi << 8) | lo
+	cpu.addr_abs += cpu.y
+
+	if (cpu.addr_abs & 0xFF00) != (hi << 8) {
+		return 1
+	}
+
+	return 0
+}
+
+
+// Instruction Implementations
+func (cpu *CPU) ADC() uint8 {
+	cpu.Fetch()
+
+	temp := uint16(cpu.a) + uint16(cpu.fetched) + uint16(cpu.GetFlag(C))
+
+	cpu.SetFlag(C, temp > 255)
+
+	cpu.SetFlag(Z, (temp & 0x00FF) == 0)
+
+	cpu.SetFlag(V, (~(uint16(cpu.a) ^ uint16(cpu.fetched)) & (uint16(cpu.a) ^ uint16(temp))) & 0x0080)
+
+	cpu.SetFlag(N, temp & 0x80)
+
+	cpu.a = temp & 0x00FF
+
+	return 1
+}
+
+func (cpu *CPU) SBC() uint8 {
+	cpu.Fetch()
+
+	value := uint16(cpu.fetched) ^ 0x00FF
+
+	temp := uint16(cpu.a) + value + uint16(cpu.GetFlag(C))
+	cpu.SetFlag(C, temp & 0xFF00)
+	cpu.SetFlag(Z, ((temp & 0x00FF) == 0))
+	cpu.SetFlag(V, (temp ^ uint16(cpu.a)) & (temp ^ value) & 0x0080)
+	cpu.SetFlag(N, temp & 0x0080)
+	cpu.a = temp & 0x00FF
+
+	return 1
+}
+
+func (cpu *CPU) AND() uint8 {
+	return 1
+}
+
+func (cpu *CPU) ASL() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BCC() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BCS() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BEQ() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BIT() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BMI() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BNE() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BPL() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BRK() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BVC() uint8 {
+	return 1
+}
+
+func (cpu *CPU) BVS() uint8 {
+	return 1
+}
+
+func (cpu *CPU) CLC() uint8 {
+	return 1
+}
+
+func (cpu *CPU) CLD() uint8 {
+	return 1
+}
+
+func (cpu *CPU) CLI() uint8 {
+	return 1
+}
+
+func (cpu *CPU) CLV() uint8 {
+	return 1
+}
+
+func (cpu *CPU) CMP() uint8 {
+	return 1
+}
+
+func (cpu *CPU) CPX() uint8 {
+	return 1
+}
+
+func (cpu *CPU) CPY() uint8 {
+	return 1
+}
+
+func (cpu *CPU) DEC() uint8 {
+	return 1
+}
+
+func (cpu *CPU) DEX() uint8 {
+	return 1
+}
+
+func (cpu *CPU) DEY() uint8 {
+	return 1
+}
+
+func (cpu *CPU) EOR() uint8 {
+	return 1
+}
+
+func (cpu *CPU) INC() uint8 {
+	return 1
+}
+
+func (cpu *CPU) INX() uint8 {
+	return 1
+}
+
+func (cpu *CPU) INY() uint8 {
+	return 1
+}
+
+func (cpu *CPU) JMP() uint8 {
+	return 1
+}
+
+func (cpu *CPU) JSR() uint8 {
+	return 1
+}
+
+func (cpu *CPU) LDA() uint8 {
+	return 1
+}
+
+func (cpu *CPU) LDX() uint8 {
+	return 1
+}
+
+func (cpu *CPU) LDY() uint8 {
+	return 1
+}
+
+func (cpu *CPU) NOP() uint8 {
+	return 1
+}
+
+func (cpu *CPU) ORA() uint8 {
+	return 1
+}
+
+func (cpu *CPU) PHA() uint8 {
+	return 1
+}
+
+func (cpu *CPU) PHP() uint8 {
+	return 1
+}
+
+func (cpu *CPU) PLA() uint8 {
+	return 1
+}
+
+func (cpu *CPU) PLP() uint8 {
+	return 1
+}
+
+func (cpu *CPU) ROL() uint8 {
+	return 1
+}
+
+func (cpu *CPU) ROR() uint8 {
+	return 1
+}
+
+func (cpu *CPU) RTI() uint8 {
+	return 1
+}
+
+func (cpu *CPU) RTS() uint8 {
+	return 1
+}
+
+func (cpu *CPU) SEC() uint8 {
+	return 1
+}
+
+func (cpu *CPU) SED() uint8 {
+	return 1
+}
+
+func (cpu *CPU) SEI() uint8 {
+	return 1
+}
+
+func (cpu *CPU) STA() uint8 {
+	return 1
+}
+
+func (cpu *CPU) STX() uint8 {
+	return 1
+}
+
+func (cpu *CPU) STY() uint8 {
+	return 1
+}
+
+func (cpu *CPU) TAX() uint8 {
+	return 1
+}
+
+func (cpu *CPU) TAY() uint8 {
+	return 1
+}
+
+func (cpu *CPU) TSX() uint8 {
+	return 1
+}
+
+func (cpu *CPU) TXA() uint8 {
+	return 1
+}
+
+func (cpu *CPU) TXS() uint8 {
+	return 1
+}
+
+func (cpu *CPU) TYA() uint8 {
+	return 1
+}
+
+func (cpu *CPU) XXX() uint8 {
+	return 0
 }
