31e95a8e7ccb20b044a108cf35ff9af68c5b3358
Morgan <58524460+hexhowells@users.noreply.github.com>
2024-01-07 21:42:15 +0000

    Update ppu
    expose read/write functions and add ConnectCartridge function

--- emu/ppu.go
+++ emu/ppu.go
@@ -1,11 +1,11 @@
 package emu
 
 type PPU struct {
-
+	cart Cartridge
 }
 
 
-func (p *PPU) cpuRead(addr uint16, bReadOnly bool) uint8 {
+func (p *PPU) CpuRead(addr uint16, bReadOnly bool) uint8 {
 	data := uint8(0x00)
 
 	switch addr {
@@ -31,7 +31,7 @@ func (p *PPU) cpuRead(addr uint16, bReadOnly bool) uint8 {
 }
 
 
-func (p *PPU) cpuWrite(addr uint16, data uint8) {
+func (p *PPU) CpuWrite(addr uint16, data uint8) {
 	switch addr {
 		case 0x0000:  // control
 			break
@@ -53,7 +53,7 @@ func (p *PPU) cpuWrite(addr uint16, data uint8) {
 }
 
 
-func (p *PPU) ppuRead(addr uint16, bReadOnly bool) uint8 {
+func (p *PPU) PpuRead(addr uint16, bReadOnly bool) uint8 {
 	data := uint8(0x00)
 	addr &= 0x3FFF
 
@@ -61,6 +61,11 @@ func (p *PPU) ppuRead(addr uint16, bReadOnly bool) uint8 {
 }
 
 
-func (p *PPU) ppuWrite(addr uint16, data uint8) {
+func (p *PPU) PpuWrite(addr uint16, data uint8) {
 	addr &= 0x3FFF
+}
+
+
+func (p *PPU) ConnectCartridge(cartridge *Cartridge) {
+	p.cart = cartridge
 }
