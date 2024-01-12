b41c898ecbcfa82905a506aa0e2c0565aef89026
Morgan <58524460+hexhowells@users.noreply.github.com>
2024-01-12 21:48:16 +0000

    create mapper interface

--- emu/mapper.go
+++ emu/mapper.go
@@ -1,5 +1,15 @@
 package emu
 
-type Mapper struct {
 
+type MapperInterface interface {
+	CpuMapRead(addr uint16, mapped_addr *uint32) bool 
+	CpuMapWrite(addr uint16, mapped_addr *uint32) bool 
+	PpuMapRead(addr uint16, mapped_addr *uint32) bool 
+	PpuMapWrite(addr uint16, mapped_addr *uint32) bool 
+}
+
+
+type Mapper struct {
+	numPrgBanks uint8
+	numChrBanks uint8
 }
