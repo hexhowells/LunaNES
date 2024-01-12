ef14b3056b6f82a732f3cf3d903e18f269210b53
Morgan <58524460+hexhowells@users.noreply.github.com>
2024-01-12 20:56:41 +0000

    add bounds checking for cartridge address range
    same as 3dbf8b04b0868e4b86c44b4395ba94a530af3a7a but for the PPU

--- emu/ppu.go
+++ emu/ppu.go
@@ -2,6 +2,8 @@ package emu
 
 type PPU struct {
 	cart Cartridge
+	nameTable [2][1024]uint8
+	paletteTable [32]uint8
 }
 
 
@@ -57,12 +59,20 @@ func (p *PPU) PpuRead(addr uint16, bReadOnly bool) uint8 {
 	data := uint8(0x00)
 	addr &= 0x3FFF
 
+	if p.cart.PpuRead(addr, data) {
+		// cartridge address range
+	}
+
 	return data
 }
 
 
 func (p *PPU) PpuWrite(addr uint16, data uint8) {
 	addr &= 0x3FFF
+
+	if p.cart.PpuWrite(addr, data) {
+		// cartridge address range
+	}
 }
 
 
